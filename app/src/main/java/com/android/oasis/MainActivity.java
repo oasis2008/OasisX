8045aeb932c9225f1794014632ecdda9d1f7625b
oasis2008 <yaoxiaochang@gmail.com>
2017-09-22 15:58:50 +0800

    Initial commit

---
+++ app/src/main/java/com/android/oasis/MainActivity.java
@@ -0,0 +1,19 @@
+package com.android.oasis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Show How to use ConstraintLayout。
+ * 1,Add  compile 'com.android.support.constraint:constraint-layout:lasted-update' to app's build.gradle.(Android Stuiod must 2.2 above)
+ * 2,
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
